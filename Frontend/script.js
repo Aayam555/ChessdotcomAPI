026b66b624ae58d2ac55cab329cd5328d632970f
Aayam555 <63101665+Aayam555@users.noreply.github.com>
2021-07-15 22:23:46 +0545

    New Features Added

--- Frontend/script.js
+++ Frontend/script.js
@@ -13,19 +13,51 @@ const updateProfileData = async () => {
 
   let htmlTemplate;
 
-  if (profileData == "Not Found"){
+  function createTemplate(image_error, not_found){
+    let htmlTemplate;
+    if (image_error){
+      htmlTemplate = `<div class="image-username">
+                         <img src="./images/questionmark.jpg" alt="profile image">
+                         <a target=_blank href="${profileDataPlayer["url"]}" id="username">${profileDataPlayer["username"]}</a>
+                      </div>
+                      <span id="name">${profileDataPlayer["name"]}</span>
+                      <span class="player-info-text" id="country_name">Country: ${profileDataPlayer["country_name"]}</span>
+                      <span class="player-info-text" id="status">Status: ${profileDataPlayer["is_online"]}</span>`;
+
+    } else if (not_found){
+      htmlTemplate = `<div class="image-username">
+                         <img src="./images/questionmark.jpg" alt="profile image">
+                         <a target=_blank href="#" id="username">Not Found</a>
+                      </div>
+                      <span id="name">Not Found</span>
+                      <span class="player-info-text" id="country_name">Country: Not Found</span>
+                      <span class="player-info-text" id="status">Status: Not Found</span>`;
+
+    } else{
     htmlTemplate = `<div class="image-username">
-                           <img src="./images/questionmark.jpg" alt="profile image">
-                           <a href="#" id="username">Not Found</a>
-                        </div>
-                        <span id="name">Not Found</span>`
-  } else{
-    htmlTemplate = `<div class="image-username">
-                           <img src="${profileDataPlayer["avatar"]}" alt="profile image">
-                           <a target=_blank href="${profileDataPlayer["url"]}" id="username">${profileDataPlayer["username"]}</a>
-                        </div>
-                        <span id="name">${profileDataPlayer["name"]}</span>
- `;
+                       <img src="${profileDataPlayer["avatar"]}" alt="profile image">
+                       <a target=_blank href="${profileDataPlayer["url"]}" id="username">${profileDataPlayer["username"]}</a>
+                    </div>
+                    <span id="name">${profileDataPlayer["name"]}</span>
+                    <span class="player-info-text" id="country_name">Country: ${profileDataPlayer["country_name"]}</span>
+                    <span class="player-info-text" id="status">Status: ${profileDataPlayer["is_online"]}</span>`;
+
+    }
+    return htmlTemplate;
   }
+
+  if (profileData == "Not Found"){
+    htmlTemplate = createTemplate(false, true);
+
+  } else{
+    if (profileDataPlayer["avatar"] == undefined){
+        htmlTemplate = createTemplate(true, false);
+
+  ;} else{
+    htmlTemplate = createTemplate(false, false);
+
+  }
+
+}
   profile.innerHTML = htmlTemplate;
 }
