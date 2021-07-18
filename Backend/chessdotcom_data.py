a3ab83cc53ff29901fc139c77aa4d47c134c1ef1
Aayam Shrestha <63101665+Aayam555@users.noreply.github.com>
2021-07-18 13:52:42 +0545

    Add files via upload

--- Backend/chessdotcom_data.py
+++ Backend/chessdotcom_data.py
@@ -1,10 +1,31 @@
 import chessdotcom
-
+import requests
 
 class Chess_info:
     def player_info(self, username):
         try:
-            return chessdotcom.get_player_profile(username)
+            info =  chessdotcom.get_player_profile(username).json
+
+            info["player"]["country_name"] = requests.get(info["player"]["country"]).json()["name"]
+
+            player_status = chessdotcom.is_player_online(username).json["online"]
+
+            if player_status:
+                info["player"]["is_online"] = "online"
+
+            else:
+                info["player"]["is_online"] = "offline"
+
+            return info
+
+        except:
+            return "Not Found"
+
+
+    def player_stat(self, username):
+        try:
+            return chessdotcom.get_player_stats(username).json
+
 
         except:
             return "Not Found"
